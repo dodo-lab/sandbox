b48d82f97f0a4cfcb7349515f78d87ba70a749a8
w-shindo <mujet88x@gmail.com>
2021-08-27 20:03:55 +0900

    ウイルスナビが見辛いので以下を対応 ・最終更新日の表示 ・最終更新日からの経過日数に応じて背景色を変更

---
+++ chrome-extention/virus-navi/content.js
@@ -0,0 +1,43 @@
+$(function () {
+  let before1Day = createBeforeDate(1);
+  let before3Day = createBeforeDate(3);
+  let before14Day = createBeforeDate(14);
+
+  let items = $('.m-searchlist-item');
+  for (let item of items) {
+    // 最終更新日の文字列抽出
+    let lastUpdate = $(item)
+      .find('.m-searchlist-item__body')
+      .children('div:first')
+      .find('p')
+      .html()
+      .replace('最終更新日 : ', '');
+
+    // 正規表現で年月日を抽出
+    let m = lastUpdate.match(/(\d+)年(\d+)月(\d+)日/);
+    // Webページ上に表示する日付
+    let displayLastUpdate = `${m[2]}/${m[3]}`;
+
+    // Webページに挿入
+    $(item)
+      .children('.m-searchlist-item__head')
+      .find('.m-searchlist-item__data')
+      .append(`<div>:${displayLastUpdate}</div>`);
+
+    // 最終更新日からの経過日によって病院の背景色を変更
+    let itemDate = new Date(m[1], m[2] - 1, m[3]);
+    if (itemDate >= before1Day) {
+      $(item).css('background-color', '#87CEFA');
+    } else if (itemDate >= before3Day) {
+      $(item).css('background-color', '#9ACD32');
+    } else if (itemDate <= before14Day) {
+      $(item).css('background-color', 'grey');
+    }
+  }
+});
+
+function createBeforeDate(beforeDay) {
+  let date = new Date();
+  date.setDate(date.getDate() - beforeDay);
+  return date;
+}
