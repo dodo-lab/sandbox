55007de27c7cc2339da6268bd02e8e1b31a5162a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-01-07 17:05:35 +0900

    ✨: ボタン押下でテキスト変更

--- android/EspressoTest/app/src/main/java/com/example/espressotest/MainActivity.kt
+++ android/EspressoTest/app/src/main/java/com/example/espressotest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.espressotest
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.espressotest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main )
+
+        binding.button.setOnClickListener {
+            binding.text.text = "Button Clicked"
+        }
     }
 }
