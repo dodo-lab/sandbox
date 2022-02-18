313d6294c950e6562a015a61e7ba90ecd9dd8baa
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-19 08:29:50 +0900

    ✨: レスポンスのインターセプト

--- axios/pages/interceptors.tsx
+++ axios/pages/interceptors.tsx
@@ -12,34 +12,49 @@ const config: AxiosRequestConfig = {
 };
 
 const normalAxios = axios.create(config);
-const interceptAxios = axios.create(config);
+const requestInterceptAxios = axios.create(config);
+const responseInterceptAxios = axios.create(config);
 
 // リクエストのインターセプト
-interceptAxios.interceptors.request.use(config => {
+requestInterceptAxios.interceptors.request.use(config => {
   return {...config, params: {zipcode: '7830061'}};
+});
+
+// レスポンスのインターセプト
+responseInterceptAxios.interceptors.response.use(response => {
+  response.data.results[0].address1 = '新潟県';
+  return response;
 });
 
 const Interceptors: React.FC = () => {
   const [normalResponse, setNormalResponse] = useState<AddressInfo | undefined>(undefined);
-  const [interceptResponse, setInterceptResponse] = useState<AddressInfo | undefined>(undefined);
+  const [requestInterceptResponse, setRequestInterceptResponse] = useState<AddressInfo | undefined>(undefined);
+  const [responseInterceptResponse, setResponseInterceptResponse] = useState<AddressInfo | undefined>(undefined);
 
-  const normalRequest = async () => {
+  const normal = async () => {
     const res = await normalAxios.get<Response>('', {params: {zipcode: '7830060'}});
     setNormalResponse(res.data.results[0]);
   };
 
-  const interceptRequest = async () => {
-    const res = await interceptAxios.get<Response>('', {params: {zipcode: '7830060'}});
-    setInterceptResponse(res.data.results[0]);
+  const requestIntercept = async () => {
+    const res = await requestInterceptAxios.get<Response>('', {params: {zipcode: '7830060'}});
+    setRequestInterceptResponse(res.data.results[0]);
+  };
+
+  const responseIntercept = async () => {
+    const res = await responseInterceptAxios.get<Response>('', {params: {zipcode: '7830060'}});
+    setResponseInterceptResponse(res.data.results[0]);
   };
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <button onClick={normalRequest}>Normal</button>
+        <button onClick={normal}>Normal</button>
         <Address addressInfo={normalResponse} />
-        <button onClick={interceptRequest}>Intercept</button>
-        <Address addressInfo={interceptResponse} />
+        <button onClick={requestIntercept}>Request Intercept</button>
+        <Address addressInfo={requestInterceptResponse} />
+        <button onClick={responseIntercept}>Response Intercept</button>
+        <Address addressInfo={responseInterceptResponse} />
       </main>
     </div>
   );
