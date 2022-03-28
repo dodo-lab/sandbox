14ee7520db333fda33fd834d0a1bfb1eff4d9979
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-28 18:49:00 +0900

    ✨: Todo一覧表示をProvider化

--- flutter/todo_app_provider/lib/todo_list_page.dart
+++ flutter/todo_app_provider/lib/todo_list_page.dart
@@ -1,42 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/providers/todo_data.dart';
 import 'package:todo_app/todo_add_page.dart';
+import 'package:provider/provider.dart';
 
-class TodoListPage extends StatefulWidget {
+class TodoListPage extends StatelessWidget {
   const TodoListPage({Key? key}) : super(key: key);
 
   @override
-  State<TodoListPage> createState() => _TodoListPageState();
-}
-
-class _TodoListPageState extends State<TodoListPage> {
-  final _todos = <String>[];
-
-  @override
-  void initState() {
-    super.initState();
-
-    _todos.addAll(['顔を洗う', '歯磨きをする', '寝る']);
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final todoData = context.watch<TodoData>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo一覧'),
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: _todos.length,
+          itemCount: todoData.todos.length,
           itemBuilder: (context, index) {
             return Card(
               child: ListTile(
-                title: Text(_todos[index]),
+                title: Text(todoData.todos[index]),
                 trailing: IconButton(
                   icon: const Icon(Icons.check),
                   onPressed: () {
-                    setState(() {
-                      _todos.removeAt(index);
-                    });
+                    todoData.complete(index);
                   },
                 ),
               ),
@@ -45,18 +33,12 @@ class _TodoListPageState extends State<TodoListPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final todo = await Navigator.of(context).push(MaterialPageRoute(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
             builder: (context) {
               return const TodoAddPage();
             },
           ));
-
-          if (todo != null) {
-            setState(() {
-              _todos.add(todo);
-            });
-          }
         },
         child: const Icon(Icons.add),
       ),
