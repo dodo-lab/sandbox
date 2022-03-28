30a176b442d52ec8ed98f01277f3a6794461aad8
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-28 11:39:57 +0900

    Todo追加画面のレイアウト

--- flutter/todo_app/lib/todo_add_page.dart
+++ flutter/todo_app/lib/todo_add_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class TodoAddPage extends StatelessWidget {
@@ -9,12 +11,34 @@ class TodoAddPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Todo追加'),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('戻る'),
+      body: Container(
+        padding: const EdgeInsets.all(64.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Todo追加'),
+              ),
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: TextButton.styleFrom(
+                  primary: Colors.red,
+                ),
+                child: const Text('キャンセル'),
+              ),
+            ),
+          ],
         ),
       ),
     );
