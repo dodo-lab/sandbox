a31ab30045918a158e69ee4b994d3108340ff20f
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-05 11:34:10 +0900

    ✅: 座標指定でタップ

--- react-native/appium/e2e/main.test.ts
+++ react-native/appium/e2e/main.test.ts
@@ -15,6 +15,7 @@ describe('main', () => {
   test('Basic画面に遷移した後、トップに戻る', async () => {
     const element = await browser.$('~linkList_0');
     await element.waitForDisplayed({timeout: 2000});
+
     element.click();
 
     await sleep(1000);
@@ -24,5 +25,33 @@ describe('main', () => {
 
     await sleep(1000);
     await browser.saveScreenshot(`./screenshot/basic_to_top.png`);
+  });
+
+  test('座標指定でBasic画面に遷移した後、トップに戻る', async () => {
+    const element = await browser.$('~linkList_0');
+    await element.waitForDisplayed({timeout: 2000});
+
+    const location = await element.getElementLocation(element.elementId);
+    const locationInView = await element.getElementLocationInView(element.elementId);
+    const size = await element.getSize();
+    const rect = await element.getElementRect(element.elementId);
+    console.log('location', location);
+    console.log('locationInView', locationInView);
+    console.log('size', size);
+    console.log('rect', rect);
+    browser.touchAction({
+      action: 'tap',
+      x: location.x + size.width * 0.5,
+      y: location.y + size.height * 0.5,
+      // Rectでも良さそう
+      // x: rect.x + rect.width * 0.5,
+      // y: rect.y + rect.height * 0.5,
+    });
+
+    await sleep(1000);
+
+    await browser.back();
+
+    await sleep(500);
   });
 });
