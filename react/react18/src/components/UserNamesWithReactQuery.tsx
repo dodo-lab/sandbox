0e85a663659dc6c2770506ea88c4a3f48b9d1990
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-15 06:44:55 +0900

    ✨: refetchとremoveをした場合の動作

--- react/react18/src/components/UserNamesWithReactQuery.tsx
+++ react/react18/src/components/UserNamesWithReactQuery.tsx
@@ -1,12 +1,32 @@
+import {Box, Button} from '@mui/material';
 import React from 'react';
 import {useQuery} from 'react-query';
 import {fetchUserNames} from 'utils/user';
 import {UserNames} from './UserNames';
 
 export const UserNamesWithReactQuery: React.FC = () => {
-  const {data} = useQuery('suspense_react_query', () => fetchUserNames(1000), {
+  const {data, refetch, remove} = useQuery('suspense_react_query', () => fetchUserNames(1000), {
     suspense: true,
+    staleTime: 0,
   });
 
-  return <UserNames userNames={data ?? []} />;
+  return (
+    <>
+      <Box sx={{mb: 2}}>
+        <Button variant="contained" onClick={() => refetch()}>
+          refetch
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ml: 2}}
+          onClick={() => {
+            remove();
+            refetch();
+          }}>
+          remove {'&'} refetch
+        </Button>
+      </Box>
+      <UserNames userNames={data ?? []} />
+    </>
+  );
 };
