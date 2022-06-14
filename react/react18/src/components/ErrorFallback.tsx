c143f5730ebb43511989bd32ed47885d41a50a77
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-15 07:31:58 +0900

    💄: エラーコンポーネントの見た目調整

--- react/react18/src/components/ErrorFallback.tsx
+++ react/react18/src/components/ErrorFallback.tsx
@@ -1,4 +1,5 @@
-import {Button, Typography} from '@mui/material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import {Box, Button, styled, Typography} from '@mui/material';
 import React, {ComponentProps} from 'react';
 import {FallbackProps} from 'react-error-boundary';
 
@@ -6,12 +7,29 @@ type Props = FallbackProps & {
   onClick?: ComponentProps<typeof Button>['onClick'];
 };
 
+const ErrorMessageBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 300,
+  padding: 20,
+  borderStyle: 'solid',
+  borderWidth: 1,
+  borderColor: 'red',
+});
+
 export const ErrorFallback: React.FC<Props> = props => {
   return (
     <>
-      <Typography>Error: {props.error.message}</Typography>
+      <ErrorMessageBox>
+        <ErrorOutlineIcon sx={{width: 36, height: 36, color: 'red'}} />
+        <Typography variant="h5" color="red">
+          {props.error.message}
+        </Typography>
+      </ErrorMessageBox>
       <Button
         variant="contained"
+        sx={{mt: 2}}
         onClick={event => {
           props.onClick?.(event);
           props.resetErrorBoundary();
