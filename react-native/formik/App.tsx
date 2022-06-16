74754bfaae3b86938c7009854640ca763244bed1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-17 06:43:25 +0900

    ⚗️: Formikとyupでバリデーション

--- react-native/formik/App.tsx
+++ react-native/formik/App.tsx
@@ -1,115 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useRef} from 'react';
+import {ScrollView, TextInput, View} from 'react-native';
+import {Button, Input} from 'react-native-elements';
+import * as Yup from 'yup';
+import {Formik, FormikValues} from './node_modules/formik/dist/index';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const initialValues = {
+  name: '',
+  age: '',
+  address: '',
 };
+
+type FormValues = typeof initialValues;
+
+type FormSchema<Values extends FormikValues> = {
+  [K in keyof Values]: Yup.AnySchema;
+};
+
+const validationSchema = () =>
+  Yup.object<FormSchema<FormValues>>({
+    name: Yup.string().required(),
+    age: Yup.string().required(),
+    address: Yup.string().required(),
+  });
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const nameRef = useRef<TextInput>(null);
+  const viewRef = useRef<View>(null);
+  const scrollViewRef = useRef<ScrollView>(null);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView ref={scrollViewRef}>
+      <Formik initialValues={initialValues} onSubmit={() => {}} validateOnBlur validationSchema={validationSchema}>
+        {props => {
+          const {values, errors, handleChange} = props;
+
+          return (
+            <>
+              <View style={{backgroundColor: 'red', height: 100}} />
+              <Input
+                label="Name"
+                value={values.name}
+                onChangeText={handleChange('name')}
+                errorMessage={errors.name}
+                ref={nameRef}
+              />
+              <View ref={viewRef} style={{backgroundColor: 'red', height: 800}} />
+              <Input label="Age" value={values.age} onChangeText={handleChange('age')} errorMessage={errors.age} />
+              <View style={{backgroundColor: 'red', height: 500}} />
+              <Input
+                label="Adress"
+                value={values.address}
+                onChangeText={handleChange('address')}
+                errorMessage={errors.address}
+              />
+              <View style={{backgroundColor: 'red', height: 500}} />
+              <Button
+                title="validate"
+                onPress={async () => {
+                  const _errors = await props.validateForm();
+                  console.log(_errors);
+
+                  console.log('measure', viewRef.current?.measure);
+                  console.log('measureInWindow', viewRef.current?.measureInWindow);
+                  console.log('measureLayout', viewRef.current?.measureLayout);
+
+                  viewRef.current?.measure((x, y, width, height, pageX, pageY) => {
+                    console.log('measure', x, y, width, height, pageX, pageY);
+                  });
+
+                  viewRef.current?.measureInWindow((x, y) => {
+                    console.log('measureInWindow', x, y);
+                  });
+                  if (scrollViewRef.current) {
+                    viewRef.current?.measureLayout(
+                      scrollViewRef.current,
+                      (left, top) => {
+                        console.log('success', left, top);
+                      },
+                      () => {
+                        console.log('failed');
+                      },
+                    );
+                  }
+                }}
+              />
+            </>
+          );
+        }}
+      </Formik>
+    </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
