264d3038ebaa26b07fe6f3eb460cc2a82e5cfeea
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-18 09:42:24 +0900

    ♻️: refactor

--- react-native/formik/App.tsx
+++ react-native/formik/App.tsx
@@ -1,8 +1,8 @@
-import React, {useRef} from 'react';
-import {TextInput, View} from 'react-native';
-import {Button, Input} from 'react-native-elements';
+import React from 'react';
+import {View} from 'react-native';
+import {Button, Input, InputProps} from 'react-native-elements';
 import * as Yup from 'yup';
-import {Formik, FormikValues} from './node_modules/formik/dist/index';
+import {Formik, FormikProps, FormikValues} from './node_modules/formik/dist/index';
 import {useScrollTo} from './useScrollTo';
 
 const initialValues = {
@@ -17,6 +17,17 @@ type FormSchema<Values extends FormikValues> = {
   [K in keyof Values]: Yup.AnySchema;
 };
 
+function inputPropsFromFormik<Values extends FormikValues>(
+  formikProps: FormikProps<Values>,
+  key: keyof Values,
+): InputProps {
+  return {
+    value: formikProps.values[key],
+    onChangeText: formikProps.handleChange(key),
+    errorMessage: formikProps.errors[key]?.toString(),
+  };
+}
+
 const validationSchema = () =>
   Yup.object<FormSchema<FormValues>>({
     name: Yup.string().required(),
@@ -26,39 +37,24 @@ const validationSchema = () =>
 
 const App = () => {
   const {ScrollTargetView, ScrollViewRoot, scrollTo} = useScrollTo<keyof FormValues>();
-  const nameRef = useRef<TextInput>(null);
-  const viewRef = useRef<View>(null);
 
   return (
     <ScrollViewRoot>
       <Formik initialValues={initialValues} onSubmit={() => {}} validateOnBlur validationSchema={validationSchema}>
         {props => {
-          const {values, errors, handleChange} = props;
-
           return (
             <>
               <View style={{backgroundColor: 'red', height: 100}} />
               <ScrollTargetView name="name">
-                <Input
-                  label="Name"
-                  value={values.name}
-                  onChangeText={handleChange('name')}
-                  errorMessage={errors.name}
-                  ref={nameRef}
-                />
+                <Input label="Name" {...inputPropsFromFormik(props, 'name')} />
               </ScrollTargetView>
-              <View ref={viewRef} style={{backgroundColor: 'red', height: 500}} />
+              <View style={{backgroundColor: 'red', height: 500}} />
               <ScrollTargetView name="age">
-                <Input label="Age" value={values.age} onChangeText={handleChange('age')} errorMessage={errors.age} />
+                <Input label="Age" {...inputPropsFromFormik(props, 'age')} />
               </ScrollTargetView>
               <View style={{backgroundColor: 'red', height: 500}} />
               <ScrollTargetView name="address">
-                <Input
-                  label="Adress"
-                  value={values.address}
-                  onChangeText={handleChange('address')}
-                  errorMessage={errors.address}
-                />
+                <Input label="Adress" {...inputPropsFromFormik(props, 'address')} />
               </ScrollTargetView>
               <View style={{backgroundColor: 'red', height: 500}} />
               <Button
@@ -73,7 +69,13 @@ const App = () => {
                   scrollTo('age');
                 }}
               />
-              <Button title="validate" onPress={() => {}} />
+              <Button
+                title="validate"
+                onPress={async () => {
+                  const errors = await props.validateForm();
+                  console.log(errors);
+                }}
+              />
             </>
           );
         }}
