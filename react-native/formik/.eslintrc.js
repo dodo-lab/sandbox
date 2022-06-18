afbc4a8c1a89201ac2483884bb0b56d623a85883
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-18 10:35:40 +0900

    🔧: eslint rule

--- react-native/formik/.eslintrc.js
+++ react-native/formik/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'prettier/prettier': 'warn',
         'no-shadow': 'off',
         'no-undef': 'off',
       },
