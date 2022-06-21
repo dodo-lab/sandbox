c4443ac65d6f152e4c9bd53951c03f0c853588c1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-22 07:04:17 +0900

    💄: レイアウト調整

--- react/zustand-sandbox/src/pages/multiple-state.tsx
+++ react/zustand-sandbox/src/pages/multiple-state.tsx
@@ -1,22 +1,26 @@
-import {Box, Card, CardContent, CardProps, Container, TextField} from '@mui/material';
+import {Box, BoxProps, Container, styled, TextField} from '@mui/material';
 import {useRenderingCount} from 'hooks/useRenderingCount';
 import type {NextPage} from 'next';
 import {useUser} from 'states/useUser';
 
-const Inputs: React.FC<CardProps> = props => {
+const ItemBox = styled(Box)({
+  borderStyle: 'solid',
+  borderColor: 'black',
+  borderWidth: 2,
+});
+
+const Inputs: React.FC<BoxProps> = props => {
   const {name, age, setName, setAge} = useUser();
   const renderingCount = useRenderingCount();
 
   return (
-    <Card {...props}>
-      <CardContent>
-        {renderingCount}
-        <Box sx={{mt: 2}}>
-          <TextField label="Name" value={name} onChange={e => setName(e.target.value)} />
-          <TextField label="Age" type="number" value={age} onChange={e => setAge(Number(e.target.value))} />
-        </Box>
-      </CardContent>
-    </Card>
+    <ItemBox {...props}>
+      {renderingCount}
+      <Box sx={{p: 2}}>
+        <TextField label="Name" value={name} onChange={e => setName(e.target.value)} />
+        <TextField label="Age" type="number" value={age} onChange={e => setAge(Number(e.target.value))} />
+      </Box>
+    </ItemBox>
   );
 };
 
