2fef1383010b038a05ada9e78a13bed92939e2af
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-29 21:59:33 +0900

    ✨: 固有のstring型を定義

--- typescript/nominal/src/index.ts
+++ typescript/nominal/src/index.ts
@@ -16,7 +16,7 @@ function isFillString(v: unknown): v is FillString {
   return isString(v) && v !== '';
 }
 
-function assertFillString(v: unknown): asserts v is FillString {
+function assertFillString<T = FillString>(v: unknown): asserts v is T {
   if (!isFillString(v)) {
     throw new Error('Should be not empty string');
   }
@@ -42,3 +42,27 @@ try {
 } catch (e) {
   console.log('fillString error');
 }
+
+type FirstName = Nominal<string, 'FirstName'>;
+type LastName = Nominal<string, 'LastName'>;
+
+function firstName(v: unknown): FirstName {
+  assertFillString<FirstName>(v);
+  return v;
+}
+function lastName(v: unknown): LastName {
+  assertFillString<LastName>(v);
+  return v;
+}
+
+function outputName(firstName: FirstName, lastName: LastName) {
+  console.log(`${firstName} ${lastName}`);
+}
+
+const first = firstName('Bob');
+const last = lastName('Alice');
+
+outputName(first, last);
+
+// @ts-expect-error : tscエラーになる.
+outputName(last, first);
