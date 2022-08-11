3ca806f7b7316ec05938670794c06430c72bcc90
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-08-12 08:18:16 +0900

    ✨: WebViewでWebページを表示

--- android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.webviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.loadUrl("http://abehiroshi.la.coocan.jp/")
     }
 }
