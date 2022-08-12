0e6fe3af45d3860ae18401078631b1fdb7d4c096
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-08-12 09:08:00 +0900

    ✨: データバインディングを適用

--- android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.webviewtest
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import androidx.databinding.DataBindingUtil
+import com.example.webviewtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initWebView()
+    }
+
+    private fun initWebView() {
         val uri = Uri.parse("https://yahoo.co.jp");
 
-        val webView = findViewById<WebView>(R.id.webview)
+        val webView = binding.webview
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = AppWebViewClient(uri.host.toString())
         webView.loadUrl(uri.toString())
