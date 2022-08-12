3603f4c248cac16ffb0e1e9db8e1090e22577c42
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-08-12 09:17:59 +0900

    ✨: back & forward

--- android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ android/WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.webviewtest
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.webviewtest.databinding.ActivityMainBinding
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initWebView()
+        buttonEvent()
     }
 
     private fun initWebView() {
@@ -23,5 +25,18 @@ class MainActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = AppWebViewClient(uri.host.toString())
         webView.loadUrl(uri.toString())
+    }
+
+    private fun buttonEvent() {
+        binding.backButton.setOnClickListener {
+            if(binding.webview.canGoBack()) {
+                binding.webview.goBack()
+            }
+        }
+        binding.forwardButton.setOnClickListener {
+            if(binding.webview.canGoForward()) {
+                binding.webview.goForward()
+            }
+        }
     }
 }
