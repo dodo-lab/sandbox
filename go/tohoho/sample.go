e38bf314d66d00e15b98604f604effd9bf5fbb2c
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-11-27 10:18:46 +0900

    ✨: type convert

--- go/tohoho/sample.go
+++ go/tohoho/sample.go
@@ -17,8 +17,19 @@ func typeAlias() {
 	fmt.Printf("time=%s\n", time)
 }
 
+func typeConvert() {
+	var n1 uint16 = 1234
+	var n2 uint32 = uint32(n1)
+	fmt.Printf("n1=%d n2=%d\n", n1, n2)
+
+	var n3 uint32 = 123456
+	var n4 uint16 = uint16(n3)
+	fmt.Printf("n3=%d n4=%d\n", n3, n4)
+}
+
 func main() {
 	fmt.Println("hello, world")
 	print()
 	typeAlias()
+	typeConvert()
 }
