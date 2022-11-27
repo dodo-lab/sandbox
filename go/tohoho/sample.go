564ef00996a665d55b10a32ccb44bea55ece43a4
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-11-27 10:06:43 +0900

    ✨: const

--- go/tohoho/sample.go
+++ go/tohoho/sample.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func print() {
 	num, str := 123, "ABC"
+	const age = 20
 
 	fmt.Print("num=", num, " str=", str, "\n")
 	fmt.Println("num=", num, " str=", str)
-	fmt.Printf("num=%d str=%s\n", num, str)
+	fmt.Printf("num=%d str=%s age=%d\n", num, str, age)
 }
 
 func main() {
