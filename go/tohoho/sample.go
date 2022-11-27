90c8059d9580dd8871a37b97d4cd7336f3afa25a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-11-27 10:01:28 +0900

    ✨: fmt.Print

--- go/tohoho/sample.go
+++ go/tohoho/sample.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func print() {
+	num := 123
+	str := "ABC"
+
+	fmt.Print("num=", num, " str=", str, "\n")
+	fmt.Println("num=", num, " str=", str)
+	fmt.Printf("num=%d str=%s\n", num, str)
+}
+
 func main() {
 	fmt.Println("hello, world")
+	print()
 }
