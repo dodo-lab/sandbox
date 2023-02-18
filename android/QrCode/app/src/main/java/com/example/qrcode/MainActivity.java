4c34721ac0186845eb09f9e45e2dbaa618e3fc9a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2023-02-19 07:01:55 +0900

    ✨: added buttons

--- android/QrCode/app/src/main/java/com/example/qrcode/MainActivity.java
+++ android/QrCode/app/src/main/java/com/example/qrcode/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.zxing_button).setOnClickListener(this);
+        findViewById(R.id.zxing_android_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.zxing_button) {
+            Snackbar.make(v, "ZXing", Snackbar.LENGTH_SHORT).show();
+        }
+        else if (v.getId() == R.id.zxing_android_button) {
+            Snackbar.make(v, "ZXing Android", Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
