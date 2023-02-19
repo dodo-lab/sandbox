441d885093cc4b4a820c23c4c4d5f04e530d2996
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2023-02-19 12:13:10 +0900

    ✨: fetch and activate

--- react-native/remote-config/src/screens/home/Home.tsx
+++ react-native/remote-config/src/screens/home/Home.tsx
@@ -1,5 +1,6 @@
+import remoteConfig from '@react-native-firebase/remote-config';
 import {useNavigation} from '@react-navigation/native';
-import React, {useCallback, useContext} from 'react';
+import React, {useCallback, useContext, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Text, ThemeContext} from 'react-native-elements';
 
@@ -7,12 +8,41 @@ export const Home: React.FC = () => {
   const navigation = useNavigation();
   const {theme} = useContext(ThemeContext);
   const onGoToInstructionButtonPress = useCallback(() => navigation.navigate('Instructions'), [navigation]);
+
+  useEffect(() => {
+    remoteConfig()
+      .setDefaults({
+        testId: 'default id',
+      })
+      .then(() => {
+        return remoteConfig().setConfigSettings({
+          fetchTimeMillis: 0,
+          minimumFetchIntervalMillis: 0,
+        });
+      })
+      .then(() => {
+        console.log('Default value set.');
+        return remoteConfig().fetchAndActivate();
+      })
+      .then(fetchedRemotely => {
+        if (fetchedRemotely) {
+          console.log('Configs were retrieved from the backend and activated.');
+        } else {
+          console.log('No configs were fetched from the backend, and the local configs were already activated');
+        }
+
+        const value = remoteConfig().getValue('testId');
+        const str = value.asString();
+        console.log('remote config value:', str);
+      })
+      .catch(_ => {});
+  }, []);
+
   return (
     <View style={StyleSheet.flatten([styles.container, {backgroundColor: theme.colors?.primary}])}>
       <Text h1 style={styles.textColor}>
         Hello, World!
       </Text>
-      <Button title="Go to instructions" onPress={onGoToInstructionButtonPress} />
     </View>
   );
 };
